b093a841e1a0e98976e010a13362cf0d9ca341bf
William Y. Feng <womogenes2@gmail.com>
2024-12-26 00:04:48 -0800

    Make basic page

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
